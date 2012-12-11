635d97c5eb0031e94d22d04bdd94b06ad3215ccd
Benjamin <bmende@bowdoin.edu>
2012-12-11 14:17:21 -0500

    made some things easier to use

--- stats.cpp
+++ stats.cpp
@@ -15,7 +15,17 @@ Stats::Stats() {
   }
 }
 
+void Stats::reset() {
 
+  for (int i = 0; i < 7; i++) {
+    for (int j = 0; j < 7; j++) {
+      statistics[i][j] = 0;
+    }
+  }
+
+
+
+}
 
 void Stats::print() {
     cout << "-----------------------------\nStats:\n-----------------------------" << endl;
@@ -54,59 +64,68 @@ void Stats::print() {
     cout << "total accuracy: " << totalAccuracy << "% (" << totalCorrect << "/" << NUM_TEST << ")\n" << endl;
 }
 
-
-int main() {
+void Stats::getWeights(int numHidden, double learnRate)
+{
 
   Data *d = new Data();
-  Stats *s = new Stats();
-
-  cout << "reading data\n";
   d->readData();
-  cout << "data read\nnow making net\n";
-  NeuralNetwork *net = new NeuralNetwork(NUM_ATTRIBUTES, 10);
-  double alpha = 0.1; //this is the learning rate
-  net->init(alpha);
-  cout << "net initialized with random weights\n";
-  
+  NeuralNetwork *net = new NeuralNetwork(NUM_ATTRIBUTES, numHidden);
+  net->init(learnRate);
+
   const vector<int> train = d->getTraining();
   const vector<int> val = d->getValidation();
-  const vector<int> test = d->getTest();
 
-  //this is trainingish
-  double meanErr = 10000, prevMeanErr = meanErr;
-  int epoch = 0;
-  double tolerance = 0.1; // I think it should be one std dev, but I havent calculated this yet.
-  while (meanErr > 0.50) {
+  double meanErr;
+
+  for (int epoch = 0; epoch < 300; epoch++) {
+    
     for (int i = 0; i < NUM_TRAIN; i++) {
       const vector<double>& lineIn = d->getData()[train[i]];
       net->backProp(lineIn, d->getCover(train[i]));
     }
     
-    prevMeanErr = meanErr;
+    
     meanErr = 0;
+    
     for (int i = 0; i < NUM_VALIDATE; i++) {
+     
       const vector<double>& lineIn = d->getData()[val[i]];
       vector<double> thing = net->forwardProp(lineIn);
       vector<double> error = net->findErrorVector(thing, d->getCover(val[i]));
       double tempError = 0;
+      
       for (int j = 0; j < error.size(); j++){
-	tempError += pow(error[j], 2);
+	tempError += pow(error[j], 2);	
       }
-      // tempError /= error.size();
-      meanErr += tempError;
-    }  
-    meanErr /= NUM_VALIDATE;
-    cout << epoch << " " << meanErr <<endl;
-    epoch++;
+      
+
+      meanErr /= NUM_VALIDATE;
+      //    cout << epoch << " " << meanErr <<endl;
+      
+      if (epoch % 100 == 0) {
+	//now we save the weights
+	stringstream weightsFileName;
+	weightsFileName << epoch/100 << "-" << numHidden << ".weights";
+	net->readWeightsToFile(weightsFileName.str());
+	
+      }
+    }
   }
+}
 
-//  string blah ("weights55-10.dat");
-//  net->readWeightsToFile(blah);
-    
-//  net->readWeightsFromFile("weights.dat");
+void Stats::testWeights(string weightFileName, int numHidden, double learnRate)
+{
+  
+  Data *d = new Data();
+  d->readData();
+  const vector<int> test = d->getTest();
 
-  //now we test
-  double correct = 0;
+  NeuralNetwork *net = new NeuralNetwork(NUM_ATTRIBUTES, numHidden);
+  net->init(learnRate);
+  net->readWeightsFromFile(weightFileName);
+
+  reset();
+
   for (int i = 0; i < NUM_TEST; i++) {
     const vector<double>& lineIn = d->getData()[test[i]];
     vector<double> result = net->forwardProp(lineIn);
@@ -120,9 +139,22 @@ int main() {
       }
     }
     int actualTree = d->getCover(test[i]);
-    s->statistics[maxIndex][actualTree - 1]++;
-    if (maxIndex == actualTree-1) correct++;
+    statistics[maxIndex][actualTree - 1]++;
   }
+  
+}
+
+int main() {
+
+  Stats *s = new Stats();
+  
+
+  
+  s->getWeights(10, 0.05);
+  
+  
+  string fileName("2-10.weights");
+  s->testWeights(fileName, 10, 0.05);
 
   s->print();
 }
--- stats.h
+++ stats.h
@@ -11,7 +11,9 @@ class Stats {
 
 
   void print();
-
+  void getWeights(int numHidden, double learnRate);
+  void testWeights(std::string weightFileName, int numHidden, double learnRate);
+  void reset();
 
   int statistics[7][7];
 };
