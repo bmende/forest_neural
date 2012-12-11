d2503bd010dc1ba408ce6eb62839deabc16af4c1
John R. Visentin <jvisenti@searles224-09.campus.bowdoin.edu>
2012-12-11 15:55:15 -0500

    we are READY

--- stats.cpp
+++ stats.cpp
@@ -4,8 +4,10 @@
 using namespace std;
 
 
-Stats::Stats() {
+Stats::Stats():d(new Data()) {
 
+
+  d->readData();
   for (int i = 0; i < 7; i++) {
     for (int j = 0; j < 7; j++) {
 
@@ -67,53 +69,33 @@ void Stats::print() {
 void Stats::getWeights(int numHidden, double learnRate)
 {
 
-  Data *d = new Data();
-  d->readData();
+  
   NeuralNetwork *net = new NeuralNetwork(NUM_ATTRIBUTES, numHidden);
   net->init(learnRate);
 
   const vector<int> train = d->getTraining();
-  const vector<int> val = d->getValidation();
 
   double meanErr;
 
-  for (int epoch = 0; epoch < 300; epoch++) {
-    
+  for (int epoch = 0; epoch <= NUM_EPOCHS; epoch++) {
+    cout << 100*(double)epoch/(double)NUM_EPOCHS << " percent trained" << endl;
     for (int i = 0; i < NUM_TRAIN; i++) {
       const vector<double>& lineIn = d->getData()[train[i]];
       net->backProp(lineIn, d->getCover(train[i]));
     }
     
-    
-    meanErr = 0;
-    
-    for (int i = 0; i < NUM_VALIDATE; i++) {
-     
-      const vector<double>& lineIn = d->getData()[val[i]];
-      vector<double> thing = net->forwardProp(lineIn);
-      vector<double> error = net->findErrorVector(thing, d->getCover(val[i]));
-      double tempError = 0;
+    if (epoch % 10 == 0) {
+      //now we save the weights
+      stringstream weightsFileName;
+      weightsFileName << numHidden << "-" << epoch << ".weights";
+      net->readWeightsToFile(weightsFileName.str());
       
-      for (int j = 0; j < error.size(); j++){
-	tempError += pow(error[j], 2);	
-      }
-      
-
-      meanErr /= NUM_VALIDATE;
-      //    cout << epoch << " " << meanErr <<endl;
-      
-      if (epoch % 100 == 0) {
-	//now we save the weights
-	stringstream weightsFileName;
-	weightsFileName << epoch/100 << "-" << numHidden << ".weights";
-	net->readWeightsToFile(weightsFileName.str());
-	
-      }
     }
   }
 }
 
-void Stats::writeToFile(string path)
+
+void Stats::writeToFile(string path, int epoch, int numHidden, double mse)
 {
     // ensure we are writing to a .csv file
     if (!path.substr(path.size()-3).compare(".csv"))
@@ -137,11 +119,11 @@ void Stats::writeToFile(string path)
         outputStream.open(path.c_str());
         
         // column headers
-        outputStream << "EPOCH, # HIDDEN, MSE, 1 ACC, 2 ACC, 3 ACC, 4 ACC, 5 ACC, 6 ACC, 7 ACC, AVG ACC, TOTAL ACC" << endl;
+        outputStream << "# HIDDEN, EPOCH, MSE, 1 ACC, 2 ACC, 3 ACC, 4 ACC, 5 ACC, 6 ACC, 7 ACC, TOTAL ACC" << endl;
     }
     
     outputStream.precision(5);
-    outputStream << "?, ?, ?, ";
+    outputStream << numHidden << ", " << epoch << ", " << mse << ", ";
     
     double totalCorrect = 0;
     for (int i = 0; i < 7; i++)
@@ -170,15 +152,38 @@ void Stats::writeToFile(string path)
 void Stats::testWeights(string weightFileName, int numHidden, double learnRate)
 {
   
-  Data *d = new Data();
-  d->readData();
+  int posHyph = weightFileName.find("-");
+  int posPer = weightFileName.find(".");
+  int epoch = atoi(weightFileName.substr(posHyph + 1, posPer - posHyph).c_str());
+
+
+
   const vector<int> test = d->getTest();
+  const vector<int> val = d->getValidation();
 
   NeuralNetwork *net = new NeuralNetwork(NUM_ATTRIBUTES, numHidden);
   net->init(learnRate);
   net->readWeightsFromFile(weightFileName);
 
   reset();
+
+
+  double meanErr = 0;
+    
+  for (int i = 0; i < NUM_VALIDATE; i++) {
+     
+    const vector<double>& lineIn = d->getData()[val[i]];
+    vector<double> thing = net->forwardProp(lineIn);
+    vector<double> error = net->findErrorVector(thing, d->getCover(val[i]));
+    double tempError = 0;
+      
+    for (int j = 0; j < error.size(); j++){
+      tempError += pow(error[j], 2);	
+    }
+    meanErr += tempError;
+  }
+
+  meanErr /= NUM_VALIDATE;
 
   for (int i = 0; i < NUM_TEST; i++) {
     const vector<double>& lineIn = d->getData()[test[i]];
@@ -195,6 +200,11 @@ void Stats::testWeights(string weightFileName, int numHidden, double learnRate)
     int actualTree = d->getCover(test[i]);
     statistics[maxIndex][actualTree - 1]++;
   }
+
+  stringstream statsFile;
+  statsFile << numHidden << "-stats.csv";
+
+  writeToFile(statsFile.str(), epoch, numHidden, meanErr);
   
 }
 
@@ -204,11 +214,12 @@ int main() {
 
 
   
-  s->getWeights(10, 0.05);
+  s->getWeights(120, 0.05);
   
-  
-  string fileName("2-10.weights");
-  s->testWeights(fileName, 10, 0.05);
-  s->print();
-  s->writeToFile("stats.csv");
+  for (int i = 0; i <= NUM_EPOCHS; i+=10) {
+    cout << 100*(double)i/(double)(NUM_EPOCHS) << " percent tested\n";
+    stringstream fileName;
+    fileName << 120 << "-" << i << ".weights";
+    s->testWeights(fileName.str(), 120, 0.05);
+  }
 }
--- stats.h
+++ stats.h
@@ -1,7 +1,7 @@
 #include "ann.h"
 
 
-
+#define NUM_EPOCHS 300
 
 class Stats {
 
@@ -14,7 +14,9 @@ class Stats {
   void getWeights(int numHidden, double learnRate);
   void testWeights(std::string weightFileName, int numHidden, double learnRate);
   void reset();
-  void writeToFile(std::string path);
+  void writeToFile(std::string path, int epoch, int numHidden, double mse);
 
+ private:
   int statistics[7][7];
+  Data *d;
 };
